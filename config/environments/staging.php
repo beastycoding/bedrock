42980af353df53e18c741f291d38a7c7df3f29b5
BeastyCoding <beastycoding@outlook.com>
2024-07-23 02:30:50 +0200

    Refactor configuration

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
- * Configuration overrides for WP_ENV === 'staging'
+ * Configuration overrides for WP_ENV === 'staging'.
  */
 
 use Roots\WPConfig\Config;
 
-/**
+/*
  * You should try to keep staging as close to production as possible. However,
  * should you need to, you can always override production configuration values
  * with `Config::define`.
